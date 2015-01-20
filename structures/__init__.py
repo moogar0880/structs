f99314b0e1a3eed144ba52ec42bd7edf9cb841ce
moogar0880 <moogar0880@gmail.com>
2015-01-19 22:31:16 -0500

    Added more metadata to setup.py

--- structures/__init__.py
+++ structures/__init__.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 
 version_info = (0, 0, 1)
+
+__title__ = 'structures'
 __author__ = 'Jon Nappi'
 __version__ = [int(x) for x in version_info]
+__build__ = 0x000001
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2015 Jonathan Nappi'
 
 try:
     from .maps import *
