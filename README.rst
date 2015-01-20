3b120d1618bf092ee5dc96ad6c89214029d8f18a
moogar0880 <moogar0880@gmail.com>
2015-01-19 22:25:58 -0500

    Improving README and setup. Added wheel support

--- README.rst
+++ README.rst
@@ -1,6 +1,42 @@
 structures
 ----------
 
+.. image:: https://pypip.in/d/structures/badge.svg
+    :target: https://pypi.python.org/pypi/structures/
+    :alt: Downloads
+
+.. image:: https://pypip.in/v/structures/badge.svg
+    :target: https://pypi.python.org/pypi/structures/
+    :alt: Latest Version
+
+.. image:: https://pypip.in/py_versions/structures/badge.svg
+    :target: https://pypi.python.org/pypi/structures/
+    :alt: Supported Python versions
+
+.. image:: https://pypip.in/implementation/structures/badge.svg
+    :target: https://pypi.python.org/pypi/structures/
+    :alt: Supported Python implementations
+
+.. image:: https://pypip.in/status/structures/badge.svg
+    :target: https://pypi.python.org/pypi/structures/
+    :alt: Development Status
+
+.. image:: https://pypip.in/wheel/structures/badge.svg
+    :target: https://pypi.python.org/pypi/structures/
+    :alt: Wheel Status
+
+.. image:: https://pypip.in/egg/structures/badge.svg
+    :target: https://pypi.python.org/pypi/structures/
+    :alt: Egg Status
+
+.. image:: https://pypip.in/format/structures/badge.svg
+    :target: https://pypi.python.org/pypi/structures/
+    :alt: Download format
+
+.. image:: https://pypip.in/license/structures/badge.svg
+    :target: https://pypi.python.org/pypi/structures/
+    :alt: License
+
 Python Data Structures for Humans
 
 Generic, pure Python implementations of some more common types of data structures.
--- setup.py
+++ setup.py
@@ -1,5 +1,26 @@
-from setuptools import setup, find_packages
-from structures import __version__
+#!/usr/bin/env python
+
+import os
+import sys
+
+import structures
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+packages = [
+    'structures',
+    'structures.maps',
+    'structures.arrays',
+]
+
+requires = []
 
 with open('README.rst') as f:
     readme = f.read()
@@ -10,15 +31,31 @@ with open('LICENSE') as f:
 
 setup(
     name='structures',
-    version=__version__,
+    version=structures.__version__,
     keywords=['structures', 'data structures'],
     long_description='\n\n'.join([readme, history, license_file]),
     description='Python Data Structures for humans.',
     author='Jon Nappi',
     author_email='moogar0880@gmail.com',
     url='https://github.com/moogar0880/structures',
-    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+    license='Apache 2.0',
     zip_safe=False,
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+    ),
     classifiers=[
         'Programming Language :: Python :: 3',
         'Topic :: Utilities :: Configuration',
