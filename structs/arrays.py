3df90c63f18b790eff6ddad3c30eb7bd5b78b2d9
moogar0880 <moogar0880@gmail.com>
2015-01-24 16:26:39 -0500

    Adding tested ParallelArray, bug fixed Arrays

--- structs/arrays.py
+++ structs/arrays.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from bisect import bisect
 from textwrap import dedent
-from collections import deque, Iterable
+from collections import deque, Iterable, Sized
 
 __author__ = 'Jon Nappi'
 __all__ = ['prev', 'BaseList', 'BitArray', 'SortedList', 'CircularArray']
@@ -119,7 +119,7 @@ class BitArray(list):
         :returns: A new :class:`BitArray` with the shifted bits
         """
         if other < 0:
-            other = -other
+            return self.__rshift__(-other)
         dq = deque(self)
         dq.rotate(-other)
         return BitArray(dq)
@@ -130,7 +130,7 @@ class BitArray(list):
         :returns: A new :class:`BitArray` with the shifted bits
         """
         if other < 0:
-            other = -other
+            return self.__lshift__(-other)
         dq = deque(self)
         dq.rotate(other)
         return BitArray(dq)
@@ -140,6 +140,10 @@ class BitArray(list):
         :const:`int` representation
         """
         return int(str(self), 2)
+
+    def __index__(self):
+        """Returns the integer representation of this :class:`BitArray`"""
+        return self.__int__()
 
     def __float__(self):
         """Returns the float representation of this :class:`BitArray`'s
@@ -187,6 +191,7 @@ class BitArray(list):
             self.extend(other)
         else:
             self.append(other)
+        return self
 
     def __eq__(self, other):
         """Custom Equivalence operator"""
@@ -240,7 +245,6 @@ class SortedList(list):
     def insert(self, p_object, *args):
         """Insert *p_object* at it's calculated index"""
         index = bisect(self, self.key(p_object))
-        print(index)
         if self._reverse and index == 0:
             index = -1
         elif self._reverse:
@@ -307,22 +311,20 @@ class LookupTable(list):
     pass
 
 
-class ParallelArray(object):
-    """https://en.wikipedia.org/wiki/Parallel_array
-    Create a dynamic number of list attributes depending on a size attribute,
-    some kind of append logic that will expand a set of parameters into the
-    lists
-from structs.arrays import ParallelArray
-p = ParallelArray('Person', ['names', 'ages'])
-p.append('John Smith', 23)
-p.names.append('Johnny Appleseed')
-p.ages.append(121)
-for item in p:
-    print(item)
+class ParallelArray(Iterable, Sized):
+    """A parallel array is a list-like data structure used for representing
+    arrays of records. It keeps a separate array for each field of the record,
+    each having the same number of elements. Thus, objects located at the same
+    index in each array are implicitly linked together to form a single record
     """
 
-    def __init__(self, typename, keys):
-        self.typename, self._keys = typename, keys
+    def __init__(self, *args, keys=()):
+        """Create a new :class:`ParallelArray` instance
+
+        :param args: Arbitrary key names
+        :param keys: Explicitly declared key names. Can be mixed with args
+        """
+        self._keys = tuple(keys) + args
         for key in self._keys:
             setattr(self, key, [])
         self.__generate_append()
@@ -351,7 +353,45 @@ for item in p:
         self.append = self.append.__get__(self, self.__class__)
 
     def __iter__(self):
+        """Return a tuple generator, which concurrently iterates over all of
+        our internal lists
+        """
         return zip(*[getattr(self, key) for key in self._keys])
+
+    def __reversed__(self):
+        """Generate a reversed tuple generator which will concurrently iterate
+        over our internal lists backwards
+        """
+        return zip(*[getattr(self, key).__reversed__() for key in self._keys])
+
+    def __getitem__(self, index):
+        """Return the tuple of concurrent items stored at *index* across each
+        of our lists
+
+        :param index: The index into the array
+        :return: A tuple of the items from each internal list at *index*
+        """
+        return tuple([getattr(self, key)[index] for key in self._keys])
+
+    def __setitem__(self, key, value):
+        """Overwrite the
+
+        :param key: The index in the arrays to overrwrite
+        :param value: The values to insert across each of our arrays. Must be
+            provided as a :const:`tuple`
+        :type: tuple
+        """
+        if not isinstance(value, tuple):
+            raise TypeError
+        for k, v in zip(self._keys, value):
+            getattr(self, k).__setitem__(key, v)
+
+    def __contains__(self, item):
+        """Determine if *item* is contained in any of our internal arrays"""
+        for tuple_data in self:
+            if item in tuple_data:
+                return True
+        return False
 
     def append(self, *args, **kwargs):
         """Add the specified arguments to the underlying parallel lists. Actual
@@ -360,28 +400,132 @@ for item in p:
         raise NotImplementedError
 
     def extend(self, iterable):
-        """Append the logical bitwise representation of the objects in
-        *iterable*
+        """Extend this :class:`ParallelArray` with an *iterable* of
+        :const:`tuple`s
+
+        :param iterable: Any iterable of tuples (can be another
+            :class:`ParallelArray`) that can be merged with this
+            :class:`ParallelArray`
         """
         for args in iterable:
             self.append(*args)
 
     def insert(self, index, p_object):
-        """Append the logical bitwise representation of *p_object* to *index*
+        """Insert *p_object* at *index* across each of our internal arrays
+
+        :param index: The index to insert into
+        :param p_object: A :const:`tuple` to be insertted into this
+            :class:`ParallelArray`
+        :type: tuple
         """
-        for i, key, obj in enumerate(zip(self._keys, p_object)):
+        if not isinstance(p_object, tuple):
+            raise TypeError
+
+        for key, obj in zip(self._keys, p_object):
             getattr(self, key).insert(index, obj)
 
+    def pop(self, index=-1):
+        """Pop the provided *index* out of all underlying arrays, and return
+        a :const:`tuple` of the values that were removed
+
+        :param index: The index to pop from each internal array. Defaults to
+            the last item
+        :return: a :const:`tuple` of the values that were removed
+        """
+        return tuple([getattr(self, key).pop(index) for key in self._keys])
+
+    def clear(self):
+        """Clear all of our internal arrays"""
+        for key in self._keys:
+            getattr(self, key).clear()
+
+    def copy(self):
+        """Create a shallow copy of this :class:`ParallelArray`
+
+        :return: A new :class:`ParallelArray` with all of the same data
+        """
+        new = ParallelArray(*self._keys)
+        for key in self._keys:
+            setattr(new, key, getattr(self, key).copy())
+        return new
+
+    def count(self, value):
+        """Return a count of the number of times that *value* appears in our
+        arrays
+
+        :param value: The value to search for
+        :return: The count of the number of times *value* appears in our arrays
+        """
+        return sum([getattr(self, key).count(value) for key in self._keys])
+
+    def remove(self, value):
+        """Remove the first entery in which *value* is found in our arrays
+
+        :param value: The value to remove
+        """
+        to_delete = None
+        for index, container in enumerate(self):
+            if value in container:
+                to_delete = index
+                break
+        if to_delete is not None:
+            self.pop(to_delete)
+
+    def reverse(self):
+        """Reverse this :class:`ParallelArray` in place"""
+        [getattr(self, key).reverse() for key in self._keys]
+
     def __len__(self):
+        """Return the length of this :class:`ParallelArray`
+
+        :return: The number of records contained in this :class:`ParallelArray`
+        """
         return len(getattr(self, self._keys[0]))
 
+    def __eq__(self, other):
+        """Determine equivalence between this :class:`ParallelArray` and
+        *other*
+
+        :param other: Another :class:`ParallelArray` or iterable of iterables
+        :return: :const:`True` if this :class:`ParallelArray` is equivalent to
+            *other*, otherwise :const:`False`
+        """
+        for data, other_data in zip(self, other):
+            if data != other_data:
+                return False
+        return True
+
+    def __ne__(self, other):
+        """Determine non-equivalence between this :class:`ParallelArray` and
+        *other*
+
+        :param other: Another :class:`ParallelArray` or iterable of iterables
+        :return: :const:`False` if this :class:`ParallelArray` is equivalent to
+            *other*, otherwise :const:`True`
+        """
+        return not self.__eq__(other)
+
     def __str__(self):
+        """str representation of this :class:`ParallelArray`"""
+        if len(self) == 0:
+            return '[]'
         out = '['
         for item in self:
             out += '{}, '.format(item)
         return out[:-2] + ']'
-
     __repr__ = __str__
+
+    def as_list(self):
+        """Return this :class:`ParallelArray` as a :const:`list`"""
+        return list(self)
+
+    def as_tuple(self):
+        """Return this :class:`ParallelArray` as a :const:`tuple`"""
+        return tuple(self)
+
+    def as_dict(self):
+        """Return this :class:`ParallelArray` as a :const:`dict`"""
+        return {k: getattr(self, k) for k in self._keys}
 
 
 class SparseArray(list):
